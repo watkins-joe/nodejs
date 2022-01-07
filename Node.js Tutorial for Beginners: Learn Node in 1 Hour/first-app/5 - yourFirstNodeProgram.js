81fda96d05c383bf4df23b7d7ac2b66111e82660
Joe Watkins <joewatkins@promaxunlimited.com>
2022-01-07 12:55:46 -0600

    finished 'your first node app' section

---
+++ Node.js Tutorial for Beginners: Learn Node in 1 Hour/first-app/5 - yourFirstNodeProgram.js
@@ -0,0 +1,20 @@
+// your first node program
+
+// run code with node using
+
+// `node <nameOfFile>`
+
+// example:
+// node app.js
+
+// node is going to give app.js
+
+// this logs `Hello <name>!` to the console
+
+// if we try to console.log(window), we get a reference error that window is not defined
+
+// remember, in node.js, we don't have the window or document objects
+
+// these are part of the runtime environment we get with browsers.
+
+// we have other objects to work with files in node, the OS, the network, and so on.
