1d06aefecde2b5f309272cb3f8b8d76666dfdd95
Joe Watkins <joewatkins@promaxunlimited.com>
2022-01-07 10:52:31 -0600

    finished 'what is node' section

---
+++ Node.js Tutorial for Beginners: Learn Node in 1 Hour/whatIsNode.js
@@ -0,0 +1,28 @@
+// what is node.js?
+
+// open source, cross platform runtime environment for executing JS code OUTSIDE of the browser
+
+// we use node to build backend services (APIs), services that power our apps.
+
+// node is IDEAL FOR building:
+// HIGHLY-SCALABLE,
+// DATA-INTENSIVE,
+// REAL-TIME applications
+
+// what is so special about node?
+
+/**
+ * 1.) node is good for prototyping and agile development
+ * 2.) can also be used for building super fast and highly-scalable services
+ * - used in production by large companies such as Uber, Walmart, PayPal, Netflix, and more
+ * - PayPal found that with their Node app, it was built with..
+ *      - twice as fast with fewer people
+ *      - 33% fewer lines of code
+ *      - 40% fewer files
+ *      - DOUBLED the number of requests served PER SECOND
+ *          - WHILE decreasing the average response time by 35%
+ * 3.) JavaScript everywhere
+ * - as a frontend dev, if you know JS, you can re-use your JS skills to transition into full-stack.
+ * - source code will be cleaner and more consistence with JS on the frontend and backend. same naming conventions, tools, and best practices
+ * 4.) Largest ecosystem of open-source libraries
+ */
